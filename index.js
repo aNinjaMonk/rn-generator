966dfe84e962de23ed92597615ae2fe44e0b79af
Abhijeet Goel <abhijeetgoel77@gmail.com>
2018-07-18 13:54:42 +0530

    Updates

---
+++ index.js
@@ -0,0 +1,8 @@
+const clear = require('clear');
+const chalk = require('chalk');
+
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+//clear();
+//console.log(chalk.blue('Hello World'));
+console.log(warning('Hello'));
