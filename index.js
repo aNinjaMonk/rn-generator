0dfdf23be18104e5e214de89a2e22ebdd58d1331
Abhijeet Goel <abhijeetgoel77@gmail.com>
2018-07-18 17:05:28 +0530

    added bin

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const clear = require('clear');
 const chalk = require('chalk');
-
+const git = require('simple-git/promise')();
 const error = chalk.bold.red;
 const warning = chalk.keyword('orange');
+const inquirer = require('inquirer');
+
+inquirer.prompt([{
+  name: 'password',
+  type: 'list',
+  message: 'Enter your email address',
+  choices: ["Twitter", "Facebook"],
+  validate: function(value) {
+    if(value.length) {
+      return true;
+    } else {
+      return "Please enter email";
+    }
+  }
+}]);
+
 //clear();
 //console.log(chalk.blue('Hello World'));
-console.log(warning('Hello'));
+//console.log(warning('Hello'));
+/*git.clone("https://github.com/aNinjaMonk/rn-boilerplate.git",(status) => {
+  console.log("status");
+});*/
+//npm install
